705231fd9167c09377e21fbd5ca9370fa1e201ae
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-05-15 11:21:52 +0530

    Added `BoostrapFormBuilder` and extension methods for Rails form builder helpers

--- lib/rails_bootstrap_form.rb
+++ lib/rails_bootstrap_form.rb
@@ -4,6 +4,7 @@
 
 require "action_view"
 require "action_pack"
+require "rails_bootstrap_form/action_view_extensions/bootstrap_form_helper"
 
 module RailsBootstrapForm
   extend ActiveSupport::Autoload
@@ -11,6 +12,7 @@ module RailsBootstrapForm
   eager_autoload do
     autoload :Configuration
     autoload :BootstrapFormOptions
+    autoload :BootstrapFormBuilder
   end
 
   class << self
@@ -26,6 +28,14 @@ module RailsBootstrapForm
       yield config
     end
   end
+
+  # Override `field_error_proc` to suppress errors coming from user defined
+  # `field_error_proc`.
+  mattr_accessor :field_error_proc
+  @@field_error_proc = proc do |html_tag, _instance_tag|
+    html_tag
+  end
+
 end
 
 require "rails_bootstrap_form/engine" if defined?(Rails)
