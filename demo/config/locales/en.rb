c3c3f604ab7a3e77ee5218446ff4ca53a0b3ecc5
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-05-15 12:27:01 +0530

    Added translations for `attributes` and updated `gitignore`

--- demo/config/locales/en.rb
+++ demo/config/locales/en.rb
@@ -6,11 +6,44 @@
   en: {
     activerecord: {
       attributes: {
-
+        country: {
+          name: "Name",
+        },
+        fruit: {
+          name: "Name",
+        },
+        skill: {
+          name: "Name",
+        },
+        user: {
+          name: "Name",
+          email: "Email address",
+          password: "Password",
+          password_confirmation: "Confirm password",
+          mobile_number: "Mobile number",
+          blog_url: "Blog URL",
+          birth_date: "Birth date",
+          fruit_id: "Favorite fruit",
+          terms: "I accept terms and conditions",
+          excellence: "Excellence",
+        },
+        user_skill: {
+          user_id: "User",
+          skill_id: "Skill"
+        },
+        address: {
+          street: "Street",
+          state: "State",
+          city: "City",
+          postal_code: "Postal code",
+          country_id: "Country"
+        },
       },
       help_texts: {
-
+        user: {
+          email: "Please use official email address"
+        }
       },
-    },
+    }
   }
 }
