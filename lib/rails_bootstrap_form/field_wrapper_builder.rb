f51d0b330df05e45bcee17abacbfaa8a181336df
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-05-22 09:46:33 +0530

    Added support to have horizontal forms for fields except `check boxes` and `radio_buttons`

--- lib/rails_bootstrap_form/field_wrapper_builder.rb
+++ lib/rails_bootstrap_form/field_wrapper_builder.rb
@@ -16,35 +16,49 @@ module RailsBootstrapForm
       label = draw_label(attribute, options, bootstrap_options)
       help_text = help_text(attribute, bootstrap_options)
 
-      if bootstrap_options.floating
-        tag.div(**field_wrapper_options(bootstrap_options)) do
-          concat(input_group_wrapper(attribute, bootstrap_options) do
-            tag.div(class: floating_label_classes(attribute)) do
-              concat(capture(&block))
-              concat(label)
-            end
-          end)
-          concat(help_text)
-        end
-      else
+      if bootstrap_options.layout_horizontal?
         tag.div(**field_wrapper_options(bootstrap_options)) do
           concat(label)
-          concat(input_group_wrapper(attribute, bootstrap_options) do
-            capture(&block)
+          concat(tag.div(class: bootstrap_options.field_col_wrapper_class) do
+            concat(input_group_wrapper(attribute, bootstrap_options) do
+              capture(&block)
+            end)
+            concat(help_text)
           end)
-          concat(help_text)
+        end
+      else
+        if bootstrap_options.floating
+          tag.div(**field_wrapper_options(bootstrap_options)) do
+            concat(input_group_wrapper(attribute, bootstrap_options) do
+              tag.div(class: floating_label_classes(attribute)) do
+                concat(capture(&block))
+                concat(label)
+              end
+            end)
+            concat(help_text)
+          end
+        else
+          tag.div(**field_wrapper_options(bootstrap_options)) do
+            concat(label)
+            concat(input_group_wrapper(attribute, bootstrap_options) do
+              capture(&block)
+            end)
+            concat(help_text)
+          end
         end
       end
     end
 
     def field_wrapper_options(bootstrap_options)
       {}.tap do |option|
-        option[:class] = field_wrapper_classes
+        option[:class] = field_wrapper_classes(bootstrap_options)
       end.merge(bootstrap_options.wrapper_options)
     end
 
-    def field_wrapper_classes
-      classes = [form_wrapper_default_class]
+    def field_wrapper_classes(bootstrap_options)
+      classes = []
+      classes << "row" if bootstrap_options.layout_horizontal?
+      classes << form_wrapper_default_class
       classes.flatten.compact
     end
 
@@ -68,7 +82,7 @@ module RailsBootstrapForm
       css_options[:class] = field_classes.flatten.compact
       css_options.merge!(required_field_options(attribute, options))
 
-      if bootstrap_options.floating
+      if (bootstrap_options.floating && !bootstrap_options.layout_horizontal?)
         css_options[:placeholder] ||= label_text(attribute, bootstrap_options)
       end
 
