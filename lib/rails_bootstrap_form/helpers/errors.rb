fdd70d8856a3f4775de5ac5dfa29d7f18f47e3dc
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-07-01 10:51:39 +0530

    Code smell fixes

--- lib/rails_bootstrap_form/helpers/errors.rb
+++ lib/rails_bootstrap_form/helpers/errors.rb
@@ -7,59 +7,56 @@ module RailsBootstrapForm
     module Errors
       extend ActiveSupport::Concern
 
-      def self.included(base_class)
-        def is_invalid?(attribute)
-          (attribute && object.respond_to?(:errors) && object.errors[attribute].any?) ||
-            has_association_error?(attribute)
+      private
+
+      def is_invalid?(attribute)
+        (attribute && object.respond_to?(:errors) && object.errors[attribute].any?) ||
+          has_association_error?(attribute)
+      end
+
+      # def input_with_error(attribute, &block)
+      #   input = capture(&block)
+      #   input << generate_error(attribute)
+      #   input
+      # end
+
+      def generate_error(attribute)
+        if is_invalid?(attribute)
+          error_text = error_messages(attribute)
+          error_klass = "invalid-feedback"
+
+          tag.div(error_text, class: error_klass)
+        end
+      end
+
+      def has_association_error?(attribute)
+        object.class.try(:reflections)&.any? do |association_name, association|
+          next unless is_belongs_to_association?(association)
+          next unless is_association_same?(attribute, association)
+
+          object.errors[association_name].any?
+        end
+      end
+
+      def error_messages(attribute)
+        messages = object.errors[attribute]
+
+        object.class.try(:reflections)&.each do |association_name, association|
+          next unless is_belongs_to_association?(association)
+          next unless is_association_same?(attribute, association)
+
+          messages << object.errors[association_name]
         end
 
-        # def input_with_error(attribute, &block)
-        #   input = capture(&block)
-        #   input << generate_error(attribute)
-        #   input
-        # end
+        messages.flatten.to_sentence
+      end
 
-        def generate_error(attribute)
-          if is_invalid?(attribute)
-            error_text = error_messages(attribute)
-            error_klass = "invalid-feedback"
+      def is_belongs_to_association?(association)
+        association.is_a?(ActiveRecord::Reflection::BelongsToReflection)
+      end
 
-            tag.div(error_text, class: error_klass)
-          end
-        end
-
-        def has_association_error?(attribute)
-          object.class.try(:reflections)&.any? do |association_name, association|
-            next unless is_belongs_to_association?(association)
-            next unless is_association_same?(attribute, association)
-
-            object.errors[association_name].any?
-          end
-        end
-
-        def error_messages(attribute)
-          messages = object.errors[attribute]
-
-          object.class.try(:reflections)&.each do |association_name, association|
-            next unless is_belongs_to_association?(association)
-            next unless is_association_same?(attribute, association)
-
-            messages << object.errors[association_name]
-          end
-
-          messages.flatten.to_sentence
-        end
-
-        def is_belongs_to_association?(association)
-          association.is_a?(ActiveRecord::Reflection::BelongsToReflection)
-        end
-
-        def is_association_same?(attribute, association)
-          (association.foreign_key == attribute.to_s)
-        end
-
-        private :is_invalid?, :generate_error, :has_association_error?,
-                :error_messages, :is_belongs_to_association?, :is_association_same?
+      def is_association_same?(attribute, association)
+        (association.foreign_key == attribute.to_s)
       end
     end
   end
